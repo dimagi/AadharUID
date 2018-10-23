a175df6415a679766935c94f28439eaa4eb9f80a
Shubham Goyal <shubham1g5@gmail.com>
2018-10-23 17:54:47 +0530

    Targets Oreo

--- app/src/main/java/org/commcarehq/aadharuid/MainActivity.java
+++ app/src/main/java/org/commcarehq/aadharuid/MainActivity.java
@@ -4,13 +4,13 @@ import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     int BARCODE_CODE = 1;
 
